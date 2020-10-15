003d906d22c5bd67a7b60608eb5a217dbd703ab3
Galen Rhodes <galenrhodes@me.com>
2020-10-14 22:07:48 -0400

    Initial Commit

---
+++ SwiftDOM/SwiftDOM.h
@@ -0,0 +1,18 @@
+//
+//  SwiftDOM.h
+//  SwiftDOM
+//
+//  Created by Galen Rhodes on 10/14/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDOM.
+FOUNDATION_EXPORT double SwiftDOMVersionNumber;
+
+//! Project version string for SwiftDOM.
+FOUNDATION_EXPORT const unsigned char SwiftDOMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDOM/PublicHeader.h>
+
+
